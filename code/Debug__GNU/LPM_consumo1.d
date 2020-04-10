6387094875db50b8357d5cc08150e05824215010
manulorente <manloralm@outlook.com>
2020-04-10 11:54:22 +0200

    First commit

---
+++ code/Debug__GNU/LPM_consumo1.d
@@ -0,0 +1,16 @@
+LPM_consumo1.d: ../LPM_consumo1.c \
+ c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430.h \
+ c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430f5529.h \
+ c:/ti/ccsv6/ccs_base/msp430/include_gcc/iomacros.h \
+ c:/ti/ccsv6/ccs_base/msp430/include_gcc/in430.h \
+ c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430f5529.h
+
+c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430.h:
+
+c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430f5529.h:
+
+c:/ti/ccsv6/ccs_base/msp430/include_gcc/iomacros.h:
+
+c:/ti/ccsv6/ccs_base/msp430/include_gcc/in430.h:
+
+c:/ti/ccsv6/ccs_base/msp430/include_gcc/msp430f5529.h:
